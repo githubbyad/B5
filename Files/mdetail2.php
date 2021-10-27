854e7945c1306f117ee2567ec89bbf4d03a62e83
Adnan <73897123+githubbyad@users.noreply.github.com>
2021-10-27 16:12:54 +0530

    Update mdetail2.php

--- Files/mdetail2.php
+++ Files/mdetail2.php
@@ -71,7 +71,7 @@ if (strlen($FileContentMain) > 0) {
     echo "<script type='text/javascript' language='javascript' src='" . $MyDomain . $DemoSite . "articles.js'></script>" . chr(13) . chr(10);
     echo "<script type='text/javascript' language='javascript' src='" . $MyDomain . $DemoSite . "customform.js'></script>" . chr(13) . chr(10);
     echo "<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>" . chr(13) . chr(10);
-    echo "<script>if (window.location.href.indexOf('mdetail2.php') != -1 && $(window).width() > 1024 && window == window.parent) {mburl = location.href;var mbar= new URL(mburl );var murl= mbar.searchParams.get('l');window.location.replace(murl);}</script>" . chr(13) . chr(10);
+    echo "<script>if (window.location.href.indexOf('mdetail2.php') != -1 && window.screen.width > 1024 && window == window.parent) {mburl = location.href;var mbar= new URL(mburl );var murl= mbar.searchParams.get('l');window.location.replace(murl);}</script>" . chr(13) . chr(10);
     echo "<script type='text/javascript' language='javascript' src='" . $MyDomain . "lib/iframeResizer.js'></script>" . chr(13) . chr(10);
     echo "<link type='text/css' rel='stylesheet' href='" . $MyDomain . $DemoSite . "custom.css'>" . chr(13) . chr(10);
     echo "<meta name='viewport' content='width=device-width, initial-scale=1' />" . chr(13) . chr(10);
